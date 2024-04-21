f74ad6ae18a4e478aa4b92a0ed1301c56837cd6d
Mitesh411 <miteshdandade@ymail.com>
2024-04-21 17:40:34 +0530

    Change URL

--- src/test/java/org/miteshdandade/Authentication.java
+++ src/test/java/org/miteshdandade/Authentication.java
@@ -7,14 +7,28 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class Authentication {
-    
+
     @Test
-    public void AuthenticationBasic(){
-        RestAssured.baseURI = "https://restapi.demoqa.com/authentication/CheckForAuthentication";
+    /**
+     * A description of the entire Java function.
+     *
+     * @param paramName description of parameter
+     * @return description of return value
+     * 
+     */
+    /**
+     * Tests basic authentication on the Unsplash API.
+     * Sends a GET request to the /users/{username} endpoint
+     * with no authentication.
+     * Validates that a 401 Unauthorized status code is returned.
+     * Prints the status code and response body to the console.
+     */
+    public void AuthenticationBasic() {
+        RestAssured.baseURI = "https://unsplash.com//users/Mitesh411";
         RequestSpecification request = RestAssured.given();
 
-        Response response= request.get();
+        Response response = request.get();
         System.out.println("Status code : " + response.getStatusCode());
-        System.out.println("Status Message : "+response.body().asString());
+        System.out.println("Status Message : " + response.body().asString());
     }
 }
